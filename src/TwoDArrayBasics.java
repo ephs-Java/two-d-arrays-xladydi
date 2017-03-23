b704f946a0e567a5011e8a291efa6fc598f6e092
Jenn Nelson <jnelson@edenpr.org>
2017-03-23 09:02:39 -0500

    initial commit

---
+++ src/TwoDArrayBasics.java
@@ -0,0 +1,68 @@
+
+public class TwoDArrayBasics {
+
+	public static void main(String[] args) {
+		// Create a 3 x 5 array of ints
+		
+		// Fill the array with random numbers using fillRandom
+		
+		// test each method you create below.
+
+	}
+	/*
+	 * given the lower and upper bounds of the random numbers
+	 * fill an array of random numbers 
+	 */
+	
+	/*
+	 * create a method that prints a 2D array
+	 */
+	
+	
+	/*
+	 * return the sum of a row (r) in a 2D array (nums)
+	 */
+	
+
+	
+	/*
+	 * return an array of row sums of a 2D array (nums)
+	 */
+	
+
+	
+	
+	/*
+	 * check an 1D array for repeats, return true if all 
+	 * elements of the array are unique
+	 */
+	
+
+	
+	
+	/*
+	 * given an array
+	 * return the index of the target value, 
+	 * return -1 if it is not found
+	 */
+	
+
+	
+	
+	/*
+	 * return the min value in a 2D array
+	 */
+	
+
+	
+	
+	
+	/*
+	 * return the max value in a 2D array
+	 */
+	
+
+	
+	
+	
+}
